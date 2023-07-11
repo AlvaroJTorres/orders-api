3154f3c07d46853898aed1d231c72b75d4c1644c
AlvaroJTorres <mangetrone@gmail.com>
2023-07-11 00:27:23 -0500

    Generate routes for users and orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users do
+    resources :orders, only: %i[create]
+  end
+  resources :orders, only: %i[index show update destroy] 
 end
